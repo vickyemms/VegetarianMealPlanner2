4b9b15e0a0f713747fa7badcce6c6727e92b84e1
vickyemms <64374691+vickyemms@users.noreply.github.com>
2024-02-26 15:21:28 +0100

    Added Top Bar

--- app/src/main/java/com/example/vegetarianmealplanner/MainActivity.kt
+++ app/src/main/java/com/example/vegetarianmealplanner/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.vegetarianmealplanner.ui.theme.VegetarianMealPlannerTheme
-import com.example.vegetarianmealplanner.view.BottomNavigationBar
+import com.example.vegetarianmealplanner.view.TopBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             VegetarianMealPlannerTheme {
-                BottomNavigationBar()
+                TopBar()
             }
         }
     }
---
+++ app/src/main/java/com/example/vegetarianmealplanner/view/TopBar.kt
@@ -0,0 +1,33 @@
+package com.example.vegetarianmealplanner.view
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Top app bar")
+                }
+            )
+        },
+    ) { padding ->
+        BottomNavigationBar()
+        Spacer(modifier = Modifier.padding(padding))
+    }
+}
