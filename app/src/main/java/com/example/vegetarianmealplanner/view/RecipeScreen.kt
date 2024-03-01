51a05875b63e51b6ab21c4d3deb984a12004ad4e
vickyemms <64374691+vickyemms@users.noreply.github.com>
2024-03-01 17:35:56 +0100

    Added LazyList for recipes in RecipeScreen

--- app/src/main/java/com/example/vegetarianmealplanner/view/RecipeScreen.kt
+++ app/src/main/java/com/example/vegetarianmealplanner/view/RecipeScreen.kt
@@ -1,8 +1,6 @@
 package com.example.vegetarianmealplanner.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,14 +9,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.example.vegetarianmealplanner.data.RecipeData
-import com.example.vegetarianmealplanner.ui.theme.Purple40
 import com.example.vegetarianmealplanner.ui.theme.VegetarianMealPlannerTheme
 
 @Composable
@@ -47,6 +42,7 @@ fun RecipeScreen(){
                     Button(onClick = { /*TODO*/ }) {
                         Text(text = "Add")
                     }
+
                 }
             }
         }
