fdd5c0c0fc03a55327a8ba8e010c2cc61514468b
vickyemms <64374691+vickyemms@users.noreply.github.com>
2024-03-01 17:35:06 +0100

    Added LazyList for recipes in RecipeScreen

--- app/src/main/java/com/example/vegetarianmealplanner/view/RecipeScreen.kt
+++ app/src/main/java/com/example/vegetarianmealplanner/view/RecipeScreen.kt
@@ -1,24 +1,55 @@
 package com.example.vegetarianmealplanner.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.vegetarianmealplanner.data.RecipeData
 import com.example.vegetarianmealplanner.ui.theme.Purple40
 import com.example.vegetarianmealplanner.ui.theme.VegetarianMealPlannerTheme
 
 @Composable
 fun RecipeScreen(){
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Purple40),
-    contentAlignment = Alignment.Center) {
-        Text(text = "Recipe", fontSize = 100.sp)
+
+    val recipes = RecipeData.recipes
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ){
+        items(recipes.size){recipe ->
+
+            val painter: Painter = painterResource(id = recipes[recipe].imageResourceId)
+
+            Column {
+                Text(text = recipes[recipe].name)
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Image(painter = painter, contentDescription = "")
+                    Column {
+                        Text(text = recipes[recipe].cuisine)
+                        Text(text = recipes[recipe].protein)
+                        Text(text = recipes[recipe].healthiness)
+                    }
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Add")
+                    }
+                }
+            }
+        }
     }
 }
 
